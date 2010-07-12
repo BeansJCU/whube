4ebd21a7b43048e4d782fd5e82a8eabfe06f9f25
Paul Tagliamonte <paultag@ubuntu.com>
2010-07-11 21:48:13 -0400

    to fix bug 9, code changes thanks to Joe Barker ( joeb454 )

--- libs/js/effects.js
+++ libs/js/effects.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
 		$('.growl').animate({ opacity: 'hide' }, 800);
 	}
 	setTimeout( hideGrr,  8000 );
+	
+	$('.message').hover(
+		function() {
+			$(this).stop().animate({"opacity": "0.60"}, 500);
+		},
+		function() {
+			$(this).stop().animate({"opacity": "1"}, 500);
+		});
 });
