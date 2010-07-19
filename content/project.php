996b0e58377fe2d18e1225477f0821e0d2832a23
tenach <tenach@whube.com>
2010-07-19 02:49:25 -0800

    Clarified bug count data

--- content/project.php
+++ content/project.php
@@ -9,6 +9,17 @@ $row = $p->getNext();
 $b->getByCol( "package", $row['pID'] ); // this is goddamn awesome
 $booboos = $b->numrows();
 
+$bugCount = 0;
+$i = 0;
+
+while ( $i < $booboos ) {
+  $bug = $b->getNext();   
+  if ( $bug['bug_status'] != 8 ) $bugCount++;
+  $i++;
+}
+
+$bugsFixed = $booboos - $bugCount;
+
 $critical = 0; // doh // $b->specialSelect( "bug_status != 1" );
 
 if ( isset ( $row['pID'] ) ) {
@@ -17,7 +28,7 @@ if ( isset ( $row['pID'] ) ) {
 <h1>" . $row['project_name'] . "</h1>
 " . $row['descr'] . "<br />
 <br />
-There are " . $booboos . " bugs in the tracker on this package. " . $critical . " are critical.
+There are " . $booboos . " bugs in the tracker on this package (" . $bugCount . " listed, " . $bugsFixed . " fixed). " . $critical . " are critical.
 ";
 } else {
 	$_SESSION['err'] = "Project " . $argv[1] . " does not exist!";
