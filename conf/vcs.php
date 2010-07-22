d336f5d22bb90dfbbbec368ce27564b38b04fc5f
Paul Tagliamonte <paultag@ubuntu.com>
2010-07-22 07:37:29 -0400

    fixing vcs stuff

--- conf/vcs.php
+++ conf/vcs.php
@@ -4,7 +4,7 @@
 
     $VCS_SITE_ENABLE     = true;
     $VCS_TYPE            = "git";
-    $VCS_SITE_PREFIX     = "http://git.whube.com/?a=summary&p=";
+    $VCS_SITE_PREFIX     = "http://localhost/git/?a=summary&p=";
     $VCS_SITE_POSTFIX    = ".git";
     $VCS_CHECKOUT_PATH   = "git@localhost:/git/";
     $VCS_CHECKOUT_CMD    = "git clone";
