97572affcb417205da88beaa5db106bc981b2aba
Paul Tagliamonte <paultag@ubuntu.com>
2010-07-28 20:34:53 -0400

    happy thymes

--- install/install.php
+++ install/install.php
@@ -9,7 +9,9 @@ Well, let's get the formalities out of the way. I don't know your name yet, dawg
 <br /><br />
 <div class = 'nametag' >
 	<div class = 'nametagfont' >
-		<input type = 'text' name = 'uname' value = '' size = '12' />
+		My full name is <input type = 'text' name = 'rname' value = '' size = '12' /><br />
+		My username is <input type = 'text' name = 'uname' value = '' size = '12' /><br />
+		My email is <input type = 'text' name = 'email' value = '' size = '1' /><br />
 	</div>
 </div>
 
