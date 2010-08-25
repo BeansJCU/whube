34fb27f78634d415ff56ca8ca397d12ccfcfb241
digitalvectorz <digitalvectorz@ubuntu.com>
2010-08-25 08:00:51 -0400

    forgot to push the updated view.php

--- view/view.php
+++ view/view.php
@@ -1,7 +1,7 @@
 <?php
 $view_root        = dirname(  __FILE__ ) . "/";
 include( $view_root . "head.php" );
-echo Format( $CONTENT );
+echo format( $CONTENT );
 include( $view_root . "foot.php" );
 
 ?>
