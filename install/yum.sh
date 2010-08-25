8bec466913cbebf9c419e9eb07f675c23968ada6
Paul Tagliamonte <paultag@ubuntu.com>
2010-08-25 15:52:55 -0400

    yum changes

--- install/yum.sh
+++ install/yum.sh
@@ -3,10 +3,10 @@
 ID=$(rpm --version)
 
 if [ "x$?" == "x0" ]; then
-	echo "You're running a dpkg system."
-	echo "Defaulting to apt-get to"
+	echo "You're running a rpm / yum system."
+	echo "Defaulting to yum to"
 	echo "install the stuff we need."
 	echo "This needs sudo rights."
 
-	yum  install httpd php mysql mysql-server php-mysql php-xml
+	sudo yum install httpd php mysql mysql-server php-mysql php-xml
 fi
