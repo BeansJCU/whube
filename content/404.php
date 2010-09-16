06f6b9fcdfdf3615a3ad43d426b178ddfa240df8
Paul Tagliamonte <paultag@ubuntu.com>
2010-09-16 17:58:46 -0400

    Gangsta!

--- content/404.php
+++ content/404.php
@@ -4,7 +4,10 @@
 $TITLE = "FFFFUUUUUUUUUU";
 $CONTENT = "
 	<h1>404'd as hell</h1>
-	<img src = '" . $SITE_PREFIX . "imgs/ffuuu.png' alt = '404' />
+	<img src = '" . $SITE_PREFIX . "imgs/ffuuu.png' alt = '404' /><br />
+	What you are looking for is really not here at all.
+	Thanks for poking around, of course.
+	Woohoo!
 ";
 
 ?>
