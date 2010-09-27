b21be78f8e09066d430d4f0b954aaf228837826a
Thomas Martin <thomas@thescoundrels.net>
2010-09-26 20:10:20 -0800

    can read users.

--- content/admin.php
+++ content/admin.php
@@ -3,8 +3,9 @@
 include( "model/user.php" );
 include( "libs/php/core.php" );
 
-requireLogin();
+// !include("tablehover.js"); - Will make things look funny.
 
+requireLogin();
 $adminMenu = "<table>
 	<tr class = 'center'>
 		<td><h2>User Administration</h2></td>
@@ -33,18 +34,55 @@ $adminMenu = "<table>
 	</tr>
 </table>";
 
-if( sizeof($argv) > 2 ) {
-	$TITLE = ucwords( $argv[2] . " " .$argv[1] );
+$users = $USER_OBJECT->getAllUsers();
+
+if( sizeof($argv) > 1 ) {
+	$TITLE = ucwords( $argv[1] . " " .$argv[2] );
 	$CONTENT = $adminMenu;
 	$CONTENT .= "<br /><h1>" . $TITLE . "</h1><br />\n";
-	
 	if( isset( $argv[3] ) ) {
+		if( $argv[1] == "read" ) {
+			if( $argv[2] == "user" ) {
+				$USER_OBJECT->getByCol("username", $argv[3]);
+				$user = $USER_OBJECT->getNext();
+				$TITLE .= " " . ucwords( $user["username"] );	
+				$CONTENT = $adminMenu;
+				$CONTENT .= "<h1>" . $TITLE . "</h1>";
+				
+				if( $user['private'] == 0 ) $private = "No";
+				if( $user['private'] == 1 ) $private = "Yes";
+				
+				// Getting exposed like a naked monkey
+				// and tabled like a bad hand of cards
+				$CONTENT .= "User ID: " . $user['uID'] ."<br />
+				Real Name: " . $user['real_name'] ."<br />
+				Username: " . $user['username'] ."<br />
+				Email: " . $user['email'] ."<br /> 
+				Locale: " . $user['locale'] ."<br />
+				Timezone: " . $user['timezone'] ."<br />
+				Private: " . $private ."<br />
+				Password: " . $user['password'] ."<br />";
+			}
+		}
 		if( $argv[2] == "create" ) {
 			$_SESSION['err'] = "You shouldn't specify a name in a URL when creating.";
 			header("Location: " . $SITE_PREFIX . "t/admin/" . $argv[1] . "/create" );
 			exit(0);
+		}	
+	}	else if( !isset( $argv[3] ) && $argv[2] == "read" ) {
+		$numUsers = count( $users );
+		$i = 0;
+		$userList = '';
+
+		while( $i < $numUsers ) {
+			$userList .= "<tr style = 'cursor:pointer' onclick=\"document.location.href = '" . $SITE_PREFIX . "t/admin/read/user/" . $users[$i]['username'] . "'\">
+											<td>" . $users[$i]['uID'] ."</td> <td><a href = '" . $SITE_PREFIX . "t/admin/read/user/" . $users[$i]['username'] . "'>" . $users[$i]['real_name'] ."</a></td> <td>" . $users[$i]['username'] ."</td> <td>" . $users[$i]['email'] ."</td> 
+										</tr>";
+			$i++;
 		}
-		$TITLE .= " " . ucwords( $argv[3] );
+		$CONTENT .= "<table class = 'sortable' >";
+		$CONTENT .= $userList;
+		$CONTENT .="</table>";
 	}
 } else {
 	$TITLE    = "Time to do the dirty!";
