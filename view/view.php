3357ffd4dfa34126129cbb594bb639bb8c93b990
Thomas Martin <tenach@gmail.com>
2011-01-26 13:13:04 -0800

    footer fixin's

--- view/view.php
+++ view/view.php
@@ -3,6 +3,6 @@ $view_root        = dirname(  __FILE__ ) . "/";
 include( $view_root . "navigation.php" );
 include( $view_root . $THEME . "/head.php" );
 echo format( $CONTENT );
-include( $view_root . $THEME . "/foot.php" );
+include( $view_root . "/foot.php" );
 
 ?>
