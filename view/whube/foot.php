a2a8e44282e1b44e7c7fb137d956a780cfcf45ee
Thomas Martin <tenach@gmail.com>
2011-01-26 12:14:30 -0800

    Simple theming GO

---
+++ view/whube/foot.php
@@ -0,0 +1,27 @@
+			</div>
+		</div>
+		<div class = "foot" >
+			Copyright (c) Whube Hackers, 2009 - <?php echo date("o"); ?><br />Peace and Love to Y'all.
+			<br />
+			<?php echo $VERSION_STRING; ?>
+			<div class = "clear" >
+			<?php echo whubeNav(); ?>
+			</div>
+		</div>
+    <?php
+    if ( isset ( $PIWIK ) && $PIWIK ) {
+      $view_root = dirname(  __FILE__ ) . "/";
+      include($view_root . "../libs/php/piwik.php");
+      $piwik = piwikJS();
+    ?>
+    <script type="text/javascript">
+    <?php
+      print $piwik["script"];
+    ?>
+    </script>
+    <noscript><p><img src="http://<?php echo $piwik["host"]; ?>/piwik.php?idsite=<?php echo $piwik["siteID"]; ?>" style="border:0" alt="" /></p></noscript>
+    <?php
+    }
+    ?>
+	</body>
+</html>
